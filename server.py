ffac5be6311b9e026c99bbba828fbbad46c02ebb
danielk <daniel.kastinen@irf.se>
2020-09-25 17:33:45 +0200

    deamon test

--- server.py
+++ server.py
@@ -4,21 +4,16 @@ import time
 from daemon import Daemon
 
 
-class YourCode(object):
+class SSTService(Daemon):
     def run(self):
         while True:
             time.sleep(1)
 
 
-class MyDaemon(Daemon):
-    def run(self):
-        # Or simply merge your code with MyDaemon.
-        your_code = YourCode()
-        your_code.run()
-
-
 if __name__ == "__main__":
-    daemon = MyDaemon('/tmp/daemon-example.pid')
+
+    daemon = SSTService('/tmp/daemon-SSTService.pid')
+
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
@@ -27,9 +22,9 @@ if __name__ == "__main__":
         elif 'restart' == sys.argv[1]:
             daemon.restart()
         else:
-            print "Unknown command"
+            print("Unknown command")
             sys.exit(2)
         sys.exit(0)
     else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
+        print("usage: %s start|stop|restart" % sys.argv[0])
         sys.exit(2)
